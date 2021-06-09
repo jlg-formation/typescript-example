a679e56074f2ede4289c1f752d6735af33874bbe
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-09 15:36:53 +0200

    dev: webserver working

--- back/src/WebServer.ts
+++ back/src/WebServer.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import {resolve} from 'path';
 
 export class WebServer {
   app!: express.Express;
+  port = 3000;
   constructor() {
     console.log('instantiate the server');
     const app = express();
+    const publicDir = resolve(__dirname, './public');
+    app.use(express.static(publicDir));
     this.app = app;
   }
 
   start() {
     console.log('start the server');
+    this.app.listen(this.port, () => {
+      console.log(`Server started on port ${this.port}`);
+    });
   }
 }
