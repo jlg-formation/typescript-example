a731706ce563d6c39baedd265089ebba00812930
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-10 10:32:36 +0200

    dev: added promises

---
+++ back/test/async/promises.spec.ts
@@ -0,0 +1,45 @@
+import {resolve} from 'path';
+import assert from 'assert';
+import {promises, readdir as readdirAsync} from 'fs';
+
+const dir = resolve(process.cwd(), './public');
+
+const readdir = (dir: string) => {
+  return new Promise<string[]>((resolve, reject) => {
+    readdirAsync(dir, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(files);
+    });
+  });
+};
+
+describe('Asynchronous', () => {
+  it('test promises code', done => {
+    let file: string;
+    let file2: string;
+
+    readdir(dir)
+      .then(files => {
+        file = resolve(dir, files[0]);
+        return promises.readFile(file);
+      })
+      .then(c1 => {
+        file2 = file + '.copy.txt';
+        return promises.writeFile(file2, c1);
+      })
+      .then(() => {
+        return promises.readFile(file2);
+      })
+      .then(c2 => {
+        assert.deepStrictEqual(c1, c2);
+        return promises.unlink(file2);
+      })
+      .then(() => {
+        done();
+      })
+      .catch(err => assert.fail(err));
+  });
+});
