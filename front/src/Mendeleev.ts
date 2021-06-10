5e85c1f270c639cf4b9a1cbbe684f4219f58b296
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-10 18:57:22 +0200

    dev: added decorator

--- front/src/Mendeleev.ts
+++ front/src/Mendeleev.ts
@@ -1,13 +1,16 @@
 import { ChemicalElt } from "./interfaces/ChemicalElt";
 import * as d3 from "d3";
+import { Logging } from "./decorators/Logging";
 
 const eltCard = {
   width: 3 + 0.5,
   height: 4 + 0.5,
 };
 
+@Logging
 export class Mendeleev {
   data: ChemicalElt[] | undefined;
+
   constructor(private div: Element) {}
 
   async init() {
@@ -32,8 +35,6 @@ export class Mendeleev {
       .append("div")
       .attr("class", "element")
       .attr("style", (d) => {
-        console.log("d: ", d);
-
         const x = (+d.Group - 1) * eltCard.width;
         const y = (+d.Period - 1) * eltCard.height;
         return `transform: translate(${x}em, ${y}em)`;
--- front/src/index.ts
+++ front/src/index.ts
@@ -4,6 +4,9 @@ import { Mendeleev } from "./Mendeleev";
 
 (async () => {
   const div = document.querySelector("div.tableau");
+  if (!div) {
+    throw new Error("cannot find the div.tableau");
+  }
   const m = new Mendeleev(div);
   await m.init();
   m.refresh();
